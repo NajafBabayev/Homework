dbd683d74a5b9e2a706aa7ccbf05a692e4ec9643
NajafBabayev <75083052+NajafBabayev@users.noreply.github.com>
2020-12-22 01:27:01 +0400

    ev tapshirigi

---
+++ Homework1/Homework/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        FirstCase:
+            Console.WriteLine("3 reqemli ededi daxil edin");
+
+            string a = Console.ReadLine();
+
+            if (a.Length != 3)
+            {
+
+                Console.WriteLine("Sehv eded daxil edilib. Eded 3 reqemli olmalidir.Zehmet olmasa bir daha 3 reqemli ededi daxil edin");
+                goto FirstCase;
+
+            }
+        SecondCase:
+            Console.WriteLine("5 reqemli ededi daxil edin");
+
+            string b = Console.ReadLine();
+
+            if (b.Length != 5)
+            {
+
+                Console.WriteLine("Sehv eded daxil edilib. Eded 5 reqemli olmalidir.Zehmet olmasa bir daha 3 reqemli ededi daxil edin");
+                goto SecondCase;
+            }
+            string c = a.Substring(0, 2) + b.Substring(1);
+            Console.WriteLine(c);
+            int c1 = Int32.Parse(c.Substring(0, 3));
+            Console.WriteLine(c1);
+            int c2 = Int32.Parse(c.Substring(3));
+            Console.WriteLine(c2);
+            int calculate = c1 - c2;
+
+            Console.WriteLine(calculate.ToString());
+
+        }
+    }
+}
---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace HM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        FirstCase:
+            Console.WriteLine("Please type a word in English.It should contain  5 letters,at least one 'B' letter and dot(.) in the end. Also, please avoid using letter 'a':");
+            string a = Console.ReadLine();
+            if (a.Length != 6)
+            {
+                goto FirstCase;
+            }
+
+            if (a.ToLower().Contains('b') || a.ToUpper().Contains('B'))
+            {
+                goto SecondCase;
+            }
+            else
+            {
+                goto FirstCase;
+            }
+        SecondCase:
+            if (a.Contains('a') || a.Contains('A'))
+            {
+                goto FirstCase;
+            }
+            if (a.Contains('.'))
+            {
+                goto ThirdCase;
+            }
+
+            else
+            {
+                goto FirstCase;
+            }
+        ThirdCase:
+            char o = 'o';
+            if (a.Contains(o))
+            {
+                int indexchar = a.IndexOf(o);
+                char upperO = Char.ToUpper(a[indexchar]);
+                a = a.Replace(o, upperO);
+            }
+            char L = 'L';
+            if (a.Contains(L))
+            {
+                int indexcharl = a.IndexOf(L);
+                char lowerL = Char.ToLower(a[indexcharl]);
+                a = a.Replace(L, lowerL);
+            }
+
+            foreach (char letter in a)
+            {
+
+                a = a.Replace("u", "k");
+                a = a.Replace("z", "");
+
+
+
+            }
+
+            Console.WriteLine(a);
+            int dot = a.Length - 1;
+            a = a.Remove(dot);
+            string ReverseA1 = a.Substring(0, 2);
+            string ReverseA2 = a.Substring(2, a.Length - 2);
+            string ReverseA = ReverseA2 + ReverseA1;
+            Console.WriteLine(ReverseA);
+        }
+
+    }
+}
